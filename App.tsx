d80b3b261786e02c5015c3ba63ff2e3e1f552a07
Bastian <nbdehlen@gmail.com>
2021-06-06 02:02:57 +0200

    types for calendar and asyncStorage check for new install on startup.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import 'reflect-metadata'
 import { NavigationContainer } from '@react-navigation/native'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { ThemeProvider } from 'react-native-magnus'
 import { DatabaseConnectionProvider } from './src/data/Connection'
 import BottomTabNavigation from './src/navigation/BottomTabNavigation'
-import TodoList from './src/components/TodoList'
-
+import { getData } from './src/utils/asyncStorage'
 const theme = {}
 
+const STORAGE_KEY = 'new_install'
+
 export const App: FunctionComponent = () => {
+  useEffect(() => {
+    const isNewInstall = async () => {
+      await getData(STORAGE_KEY).then((x) => console.log('isNewInstall', x))
+    }
+    isNewInstall()
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <DatabaseConnectionProvider>
