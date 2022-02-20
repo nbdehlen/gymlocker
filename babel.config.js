bf67ce170f202a694c166ba4223d157b5791da5d
Bastian <nbdehlen@gmail.com>
2022-02-20 01:43:27 +0100

    Change eslint and prettier plugins and rules.

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,12 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    // from old project. Likely for typeorm
-    plugins: ['@babel/transform-react-jsx-source', 'babel-plugin-transform-typescript-metadata'],
+    plugins: [
+      // Added module-resolver array for some resolving typescript bug via stackoverflow (forgot)
+      ['module-resolver', { extensions: ['.tsx', '.ts', '.js', '.json'] }],
+      '@babel/transform-react-jsx-source',
+      'babel-plugin-transform-typescript-metadata',
+      'react-native-reanimated/plugin'
+    ]
   }
 }
