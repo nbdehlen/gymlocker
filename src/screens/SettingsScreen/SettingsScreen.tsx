b14deae70f70f329d0c69beeb692ac7fc7471446
Bastian <nbdehlen@gmail.com>
2022-03-05 19:33:58 +0100

    Increase granularity in seeding.

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, { FunctionComponent, useCallback, useState } from 'react'
 import { useDatabaseConnection } from '../../data/Connection'
 import { Button, Div, Icon, Input, Text } from 'react-native-magnus'
 import sampleData from '../../data/seeding/sampleData'
@@ -7,6 +7,26 @@ import getRandomDateWithinPeriod from '../../data/seeding/utils/getRandomDateWit
 import { add, sub } from 'date-fns'
 import { SetModel } from '../../data/entities/SetModel'
 import { SetRepository } from '../../data/repositories/SetRepository'
+import { WorkoutModel } from '../../data/entities/WorkoutModel'
+import { ExerciseModel } from '../../data/entities/ExerciseModel'
+import { CardioModel } from '../../data/entities/CardioModel'
+import { ICreateCardioData } from '../../data/repositories/CardioRepository'
+import { ICreateExerciseData } from '../../data/repositories/ExerciseRepository'
+
+const randomIntFromInterval = (min: number, max: number): number => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const randomIntsFromInterval = (min: number, max: number, count: number): number[] => {
+  return [...Array(count)].map((_) => randomIntFromInterval(min, max))
+}
+
+enum WorkoutEnum {
+  EXERCISE = 'exercise',
+  CARDIO = 'cardio',
+  MIX = 'mix'
+}
 
 type OwnProps = {}
 
@@ -14,46 +34,210 @@ type Props = OwnProps
 
 export const SettingsScreen: FunctionComponent<Props> = () => {
   const { cardioRepository, workoutRepository, exerciseRepository, setRepository } = useDatabaseConnection()
-  const [id, setId] = useState<number>(1)
-  const [error, setError] = useState('')
-  const { cardios, exercises, sets } = sampleData
+  // const [count, setCount] = useState<number>(1) // should be named count
+  const [workoutCount, setWorkoutCount] = useState(1)
+  const [workoutsFrom, setWorkoutsFrom] = useState(90)
 
-  const createWorkouts = async () => {
-    const startingPeriod = sub(new Date(), { days: 90, hours: 0 })
-    const endingPeriod = new Date()
+  // const { cardios, exercises, sets } = sampleData
 
-    const promises = [...Array(id)].map(async (_) => {
-      const start = getRandomDateWithinPeriod(startingPeriod, endingPeriod)
-      const end = add(start, { days: 0, hours: 2 })
-      const workout = await workoutRepository.create({ start, end })
+  // const createWorkouts = async () => {
+  //   const startingPeriod = sub(new Date(), { days: 90, hours: 0 })
+  //   const endingPeriod = new Date()
 
-      if (workout) {
-        const newExercises = exercises.map((exercise) => ({
-          ...exercise,
-          workout_id: workout.id
-        }))
+  //   const promises = [...Array(id)].map(async (_) => {
+  //     const start = getRandomDateWithinPeriod(startingPeriod, endingPeriod)
+  //     const end = add(start, { days: 0, hours: 2 })
+  //     const workout = await workoutRepository.create({ start, end })
+  //     return workout
 
-        const createExercises = await exerciseRepository.createMany(newExercises)
+  //     if () {
+  //       const newExercises = exercises.map((exercise) => ({
+  //         ...exercise,
+  //         workout_id: workout.id
+  //       }))
 
-        if (createExercises) {
-          createExercises.map(async (exercise: any, i: number) => {
-            let setsWithExerciseIds: any = []
-            sets[i].map((set) => {
-              setsWithExerciseIds.push({ exercise_id: exercise.id, ...set })
-            })
+  //       const createExercises = await exerciseRepository.createMany(newExercises)
 
-            await setRepository.createMany(setsWithExerciseIds)
+  //       if (createExercises) {
+  //         createExercises.map(async (exercise: any, i: number) => {
+  //           let setsWithExerciseIds: any = []
+  //           sets[i].map((set) => {
+  //             setsWithExerciseIds.push({ exercise_id: exercise.id, ...set })
+  //           })
+
+  //           await setRepository.createMany(setsWithExerciseIds)
+  //         })
+  //       } else {
+  //         setError('exerciseId does not exist')
+  //       }
+  //     } else {
+  //       setError('workoutId does not exist')
+  //     }
+  //   })
+
+  //   await Promise.all[promises]
+  // }
+
+  const createWorkouts = useCallback(
+    async (count: number, startingFrom: number): Promise<WorkoutModel[]> => {
+      const startingPeriod = sub(new Date(), { days: startingFrom, hours: 0 })
+      const endingPeriod = new Date()
+
+      const promises = [...Array(count)].map(async (_): Promise<WorkoutModel> => {
+        const start = getRandomDateWithinPeriod(startingPeriod, endingPeriod)
+        // TODO: This needs fixin
+        const end = add(start, { days: 0, hours: 2 })
+        const workout = await workoutRepository.create({ start, end })
+        return workout
+      })
+
+      const workouts = await Promise.all(promises)
+      return workouts
+    },
+    [workoutRepository]
+  )
+
+  const addExercisesToWorkout = useCallback(
+    async (workout: WorkoutModel, exerciseIndexes: number[]): Promise<ExerciseModel[]> => {
+      const newExercises: ICreateExerciseData[] = []
+      sampleData.exercises.forEach((exercise, i) => {
+        if (exerciseIndexes.includes(i)) {
+          newExercises.push({
+            ...exercise,
+            workout_id: workout.id
           })
-        } else {
-          setError('exerciseId does not exist')
         }
-      } else {
-        setError('workoutId does not exist')
-      }
-    })
+      })
 
-    await Promise.all[promises]
+      if (newExercises?.length > 0) {
+        const createExercises = await exerciseRepository.createMany(newExercises)
+        return createExercises
+      }
+      return []
+    },
+    [exerciseRepository]
+  )
+
+  const addSetsToExercise = useCallback(
+    async (exercise: ExerciseModel, setArray: number): Promise<SetModel[]> => {
+      let setsWithExerciseIds: any[] = []
+      sampleData.sets[setArray].forEach((set) => {
+        setsWithExerciseIds.push({ exercise_id: exercise.id, ...set })
+      })
+      const sets = await setRepository.createMany(setsWithExerciseIds)
+      return sets
+    },
+    [setRepository]
+  )
+
+  const addCardiosToWorkout = useCallback(
+    async (workout: WorkoutModel, cardioIndexes: number[]): Promise<CardioModel[] | []> => {
+      const newCardios: ICreateCardioData[] = []
+      sampleData.cardios.forEach((cardio, i) => {
+        if (cardioIndexes.includes(i)) {
+          newCardios.push({
+            ...cardio,
+            workout_id: workout.id
+          })
+        }
+      })
+
+      if (newCardios.length > 0) {
+        const createCardio = await cardioRepository.createMany(newCardios)
+        return createCardio
+      }
+      return []
+    },
+    [cardioRepository]
+  )
+
+  const addExercisesToWorkouts = useCallback(
+    async (workouts: WorkoutModel[]) => {
+      const exercisePromises = workouts.map(async (workout) => {
+        const exerciseCount = randomIntFromInterval(1, 6)
+        const exerciseIndexes = randomIntsFromInterval(0, sampleData.exercises.length - 1, exerciseCount)
+
+        const exercises = await addExercisesToWorkout(workout, exerciseIndexes)
+        return exercises
+      })
+
+      const exercises = await Promise.all(exercisePromises)
+      const res = exercises.flat()
+      // console.log({ res })
+      return res
+    },
+    [addExercisesToWorkout]
+  )
+
+  const addSetsToExercises = useCallback(
+    async (exercises: ExerciseModel[]) => {
+      let setArray: number
+      let prevExerciseId: number
+
+      const setsPromises = exercises.map(async (exercise) => {
+        // setArray update only on exercise change
+        if (exercise.id !== prevExerciseId) {
+          prevExerciseId = exercise.id
+          setArray = randomIntFromInterval(0, sampleData.sets.length - 1)
+        }
+        return await addSetsToExercise(exercise, setArray)
+      })
+      const sets = await Promise.all(setsPromises)
+      const res = sets.flat()
+      return res
+    },
+    [addSetsToExercise]
+  )
+
+  const addCardiosToWorkouts = useCallback(
+    async (workouts: WorkoutModel[]) => {
+      const workoutPromises = workouts.map(async (workout) => {
+        const cardiosCount = randomIntFromInterval(1, 3)
+        const cardioIndexes = randomIntsFromInterval(0, sampleData.cardios.length - 1, cardiosCount)
+        return await addCardiosToWorkout(workout, cardioIndexes)
+      })
+
+      const cardios = await Promise.all(workoutPromises)
+      const res = cardios.flat()
+      return res
+    },
+    [addCardiosToWorkout]
+  )
+
+  const getWorkoutsByIds = async (workouts: WorkoutModel[]) => {
+    const promises = workouts.map(async (workout) => await getWorkoutById(workout.id))
+    const res = await Promise.all(promises)
+    const resFlat = res.flat()
+    return resFlat
   }
+
+  const createCompleteWorkoutsInSteps = useCallback(
+    async (type: WorkoutEnum): Promise<void> => {
+      const workouts = await createWorkouts(workoutCount, workoutsFrom)
+
+      if ([WorkoutEnum.EXERCISE, WorkoutEnum.MIX].includes(type)) {
+        const exercises = await addExercisesToWorkouts(workouts)
+        await addSetsToExercises(exercises)
+      }
+      if ([WorkoutEnum.CARDIO, WorkoutEnum.MIX].includes(type)) {
+        await addCardiosToWorkouts(workouts)
+      }
+      // const res = await getWorkoutsByIds(workouts)
+    },
+    [createWorkouts, addExercisesToWorkouts, addSetsToExercises, addCardiosToWorkouts, workoutCount, workoutsFrom]
+  )
+
+  // const getExerciseById = async () => {
+  //   await exerciseRepository
+  //     .getById(id, ['sets'])
+  //     .then((exercise) => exercise && console.log(JSON.stringify(exercise, null, 4)))
+  // }
+
+  // const getSetById = async () => {
+  //   await workoutRepository
+  //     .getById(id, ['exercises', 'exercises.sets'])
+  //     .then((workout) => workout && console.log(JSON.stringify(workout, null, 4)))
+  // }
 
   const getAllWorkouts = async () => {
     await workoutRepository
@@ -61,23 +245,11 @@ export const SettingsScreen: FunctionComponent<Props> = () => {
       .then((workouts) => workouts && console.log(JSON.stringify(workouts, null, 4)))
   }
 
-  const getWorkoutById = async () => {
-    await workoutRepository
-      .getById(id, ['exercises', 'exercises.sets'])
-      .then((workout) => workout && console.log(JSON.stringify(workout, null, 4)))
+  const getWorkoutById = async (id: number) => {
+    const workout = await workoutRepository.getById(id, ['exercises', 'exercises.sets', 'cardios'])
+    // .then((workout) => workout && console.log(JSON.stringify(workout, null, 4)))
+    return workout
   }
-
-  const getExerciseById = async () => {
-    await exerciseRepository
-      .getById(id, ['sets'])
-      .then((exercise) => exercise && console.log(JSON.stringify(exercise, null, 4)))
-  }
-
-  // const getSetById = async () => {
-  //   await workoutRepository
-  //     .getById(id, ['exercises', 'exercises.sets'])
-  //     .then((workout) => workout && console.log(JSON.stringify(workout, null, 4)))
-  // }
 
   const deleteAllWorkouts = async () => {
     await workoutRepository.deleteAll()
@@ -122,23 +294,22 @@ export const SettingsScreen: FunctionComponent<Props> = () => {
   //   }
   // }
 
-  const onPressInc = () => {
-    setId((prevState) => prevState + 1)
-  }
-  const onPressDec = () => {
-    setId((prevState) => prevState - 1)
-  }
+  const onPressCreateExerciseWorkouts = () => createCompleteWorkoutsInSteps(WorkoutEnum.EXERCISE)
+  const onPressCreateCardioWorkouts = () => createCompleteWorkoutsInSteps(WorkoutEnum.CARDIO)
+  const onPressCreateMixWorkouts = () => createCompleteWorkoutsInSteps(WorkoutEnum.MIX)
+  const onPressInc = () => setWorkoutCount((prevState) => prevState + 1)
+  const onPressDec = () => setWorkoutCount((prevState) => prevState - 1)
 
   return (
     <Div flex={1} justifyContent="center" alignItems="center" bg={theme.primary.color}>
       <Div flexDir="row">
-        {error ? <Text> {error} </Text> : <B.Spacer h={40} />}
+        <B.Spacer h={40} />
         <Button onPress={onPressInc} w={40} h={40} p={0} m={0} bg={theme.primary.onColor} rounded="circle">
           <Icon fontSize={24} fontFamily="AntDesign" name="plus" color={theme.light_1} />
         </Button>
         <B.Spacer w={8} />
         <Text color="white" fontSize={24}>
-          {id}
+          {workoutCount}
         </Text>
         <B.Spacer w={8} />
         <Button onPress={onPressDec} w={40} h={40} p={0} m={0} rounded="circle">
@@ -146,8 +317,16 @@ export const SettingsScreen: FunctionComponent<Props> = () => {
         </Button>
       </Div>
       <B.Spacer h={16} />
-      <Button onPress={createWorkouts} alignSelf="center" w={200}>
-        <Text color="white">Seed DB with {id} full workouts.</Text>
+      <Button onPress={onPressCreateExerciseWorkouts} alignSelf="center">
+        <Text color="white">Add {workoutCount} random strength workouts</Text>
+      </Button>
+      <B.Spacer h={16} />
+      <Button onPress={onPressCreateCardioWorkouts} alignSelf="center">
+        <Text color="white">Add {workoutCount} random cardio workouts</Text>
+      </Button>
+      <B.Spacer h={16} />
+      <Button onPress={onPressCreateMixWorkouts} alignSelf="center">
+        <Text color="white">add {workoutCount} random mixed focus workouts</Text>
       </Button>
 
       <B.Spacer h={16} />
@@ -155,15 +334,15 @@ export const SettingsScreen: FunctionComponent<Props> = () => {
         <Text color="white">Log all workouts</Text>
       </Button>
 
-      <B.Spacer h={16} />
+      {/* <B.Spacer h={16} />
       <Button onPress={getWorkoutById} alignSelf="center">
         <Text color="white">Get workout by id {id}</Text>
-      </Button>
+      </Button> */}
 
-      <B.Spacer h={16} />
+      {/* <B.Spacer h={16} />
       <Button onPress={getExerciseById} alignSelf="center">
         <Text color="white">Get exercise by id {id}</Text>
-      </Button>
+      </Button> */}
 
       <B.Spacer h={16} />
       <Button onPress={deleteAllWorkouts} alignSelf="center">
