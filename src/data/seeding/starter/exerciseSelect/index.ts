937852f10c7c0d074a4b182dac596d3c578aa23e
Bastian <nbdehlen@gmail.com>
2022-04-10 00:43:44 +0200

    Add new exercises, modifiers and muscles.

---
+++ src/data/seeding/starter/exerciseSelect/index.ts
@@ -0,0 +1,30 @@
+import abs from './abs.json'
+import back from './back.json'
+import biceps from './biceps.json'
+import calves from './calves.json'
+import chest from './chest.json'
+import delt from './delt.json'
+import forearms from './forearms.json'
+import glutes from './glutes.json'
+import hams from './hams.json'
+import lats from './lats.json'
+import quads from './quads.json'
+import traps from './traps.json'
+import triceps from './triceps.json'
+
+// TODO: This looks bad but can't be bothered rn.
+export const exerciseSelect = [
+  ...abs.abs,
+  ...back.back,
+  ...biceps.biceps,
+  ...calves.calves,
+  ...chest.chest,
+  ...delt.delt,
+  ...forearms.forearms,
+  ...glutes.glutes,
+  ...hams.hams,
+  ...lats.lats,
+  ...quads.quads,
+  ...traps.traps,
+  ...triceps.triceps,
+]
